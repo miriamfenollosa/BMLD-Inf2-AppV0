a414645c607b9f93658df797d5c2c64386a9d155
Miriam Fenollosa <fenolmir@students.zhaw.ch>
2026-02-25 09:26:11 +0100

    E-Mail angepasst

--- views/home.py
+++ views/home.py
@@ -7,7 +7,7 @@ st.title("Meine erste Streamlit App")
 """
 Diese App wurde von folgenden Personen entwickelt:
 - Miriam Fenollosa Muñoz  (fenolmir@students.zhaw.ch)
-- Laura Gjugja (gjugjlau.students.zhaw.ch)
+- Laura Gjugja (gjugjlau@students.zhaw.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
