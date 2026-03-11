205925e6f2a1632ca0ff26b7261b40c0ac3884dd
Michelle <assadmic@students.zhaw.ch>
2026-03-11 09:21:20 +0100

    Speicher

--- functions/notenberechner.py
+++ functions/notenberechner.py
@@ -1,8 +1,15 @@
+import datetime
+
+import pytz
+
+
 def berechne_note(punkte, max_punkte):
     if max_punkte == 0:
         return "Maximale Punktzahl darf nicht 0 sein"
     
     prozent = (punkte / max_punkte) * 5 + 1
-    return round(prozent, 1)
 
-    return round(prozent, 1)
+    return {
+    "timestamp": datetime.now(pytz.timezone('Europe/Zurich')).isoformat(),
+    "note": round(prozent, 1)
+    }
